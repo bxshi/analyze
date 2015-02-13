0b93033c9346eed517e105282fe99303e9c57fc8
bxshi <baoxu.shi@gmail.com>
2015-02-12 22:49:42 -0500

    move settings to CLI arguments

--- src/main/scala/edu/nd/dsg/bshi/Main.scala
+++ src/main/scala/edu/nd/dsg/bshi/Main.scala
@@ -7,21 +7,33 @@ import org.apache.spark.graphx._
 
 object Main {
 
-  val alpha = 0.15
-  val queryId = 1326293l
+  var alpha = 0.15
+  var queryId = 0l
+  var filePath = ""
+  var maxIter = 0
+  var topK = 20
 
   def main(args: Array[String]): Unit = {
+    if (args.length < 5) {
+      println("usage: alpha queryId maxIter topK filePath")
+      return
+    } else {
+      alpha = args(0).toDouble
+      queryId = args(1).toLong
+      maxIter = args(2).toInt
+      topK = args(3).toInt
+      filePath = args(4)
+    }
+
     val conf = new SparkConf()
       .setMaster("local[2]")
-      .setAppName("Simple Application")
+      .setAppName("Citation PageRank")
       .set("spark.executor.memory", "1g")
       .set("spark.driver.memory", "1g")
       .set("spark.driver.maxResultSize", "1g")
     val sc = new SparkContext(conf)
 
-    val filePath = "hdfs://dsg1.crc.nd.edu/user/bshi/dblp/citation_edge.txt"
-//    val filePath = "/Users/bshi/exges.txt"
-
+    // Input file should be space separated e.g. "src dst", one edge per line
     val file = sc.textFile(filePath).map(x => x.split(" ").map(_.toInt))
 
     val vertices: RDD[(VertexId, (Double,Boolean))] = sc.parallelize(file.map(_.toSet).reduce(_++_).toSeq.map(x => (x.toLong, (0.0, false))))
@@ -34,27 +46,29 @@ object Main {
     val graph = Graph(vertices, edges)
 
     val totalV = graph.numVertices
+    val totalE = graph.numEdges
 
-    val zeroDeg = graph.outDegrees.filter(_==0l).count()
+    val zeroDeg = graph.outDegrees.filter(_._2==0l).count()
 
-      println(zeroDeg,"totalV",totalV)
-
-    //TODO: Add edges to query point
+    Seq("Graph statistics:",
+      "--Vertices:"+totalV,
+      "--Edges"+totalE,
+      "--OutDeg==0:"+zeroDeg).foreach(println)
 
     var rankGraph: Graph[(Double, Int), Double] = graph
       // Associate the degree with each vertex
       .outerJoinVertices(graph.outDegrees) { (vid, vdata, deg) => deg.getOrElse(0) }
       // Set the weight on the edges based on the degree
-//      .mapTriplets( e => 1.0, TripletFields.Src )
       .mapTriplets( e => 1.0 / e.srcAttr, TripletFields.Src )
       // Set the vertex attributes to the initial pagerank values
       .mapVertices( (id, attr) => {
+      // At initialization stage, only source has weight
       if (id == queryId) totalV.toDouble else 0.0
     } ).outerJoinVertices(graph.inDegrees) { (vid, vdata, inDeg) => (vdata, inDeg.getOrElse(0))}
 
     var iteration = 0
     var prevRankGraph: Graph[(Double, Int), Double] = null
-    while (iteration < 50) {
+    while (iteration < maxIter) {
       rankGraph.cache()
 
       // Compute the outgoing rank contributions of each vertex, perform local preaggregation, and
@@ -67,6 +81,7 @@ object Main {
       // edge partitions.
       prevRankGraph = rankGraph
       rankGraph = rankGraph.joinVertices(rankUpdates) {
+        // Set restart probability to personalized version(all restart will direct to source node)
         (id, oldRank, msgSum) => ((if (id == queryId) alpha * totalV else 0 ) + (1.0 - alpha) * msgSum / oldRank._2, oldRank._2)
       }.cache()
 
@@ -77,18 +92,11 @@ object Main {
 
       iteration += 1
 
-//      val tmpGraph = rankGraph.outerJoinVertices(rankGraph.inDegrees){
-//        (id, attr, inDeg) => inDeg match {
-//          case Some(inDeg) => attr / inDeg
-//          case None => attr
-//        }
-//      }
-
-
     }
 
-    sc.parallelize(rankGraph.vertices.map(x => (x._2, x._1)).top(20)).saveAsTextFile("hdfs://dsg1.crc.nd.edu/user/bshi/dblp/personalized_015_"+iteration+"_top20")
+    val finalRank = rankGraph.vertices.map(x => x._2).top(topK).map(y => (y._2, y._1)).toSeq
 
+    finalRank.indices.foreach(ind => println(ind, finalRank(ind)))
 
     sc.stop()
   }
