58b8daa5e8155d56f68b44c44ed4e4b7911ab69a
bxshi <baoxu.shi@gmail.com>
2015-02-24 00:22:16 -0500

    A more detailed template

--- src/main/scala/edu/nd/dsg/bshi/ForwardBackwardPPRRanking.scala
+++ src/main/scala/edu/nd/dsg/bshi/ForwardBackwardPPRRanking.scala
@@ -1,5 +1,8 @@
 package edu.nd.dsg.bshi
 
+import org.apache.spark.SparkContext
+import org.apache.spark.graphx.Graph
+
 /**
  * Implement Forward-Backward PPR here
  * Run FPPR first, get topK and log their values as (s_1, s_2, ..., s_k),
@@ -10,5 +13,24 @@ package edu.nd.dsg.bshi
 object ForwardBackwardPPRRanking {
 
   //TODO: Implement functions to run F-B PPR
+
+  var graph: Graph = null
+
+  /**
+   * Load graph and save to graph variable
+   * @param sc SparkContext
+   * @param filePaths A list of files that will be loaded
+   */
+  def graphLoader(sc: SparkContext, filePaths: Seq[String]): Unit = {
+    //TODO: Implement graph loader
+    graph = null
+  }
+
+  /**
+   * Run FBPPR
+   */
+  def run(): Unit = {
+
+  }
 
 }
