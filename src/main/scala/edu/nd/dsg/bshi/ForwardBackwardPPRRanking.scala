76773493ccfcdc24027653a162948820aaa0cc29
bxshi <baoxu.shi@gmail.com>
2015-02-24 00:32:09 -0500

    template completed

--- src/main/scala/edu/nd/dsg/bshi/ForwardBackwardPPRRanking.scala
+++ src/main/scala/edu/nd/dsg/bshi/ForwardBackwardPPRRanking.scala
@@ -1,7 +1,11 @@
 package edu.nd.dsg.bshi
 
+import java.io.{File, PrintWriter}
+
 import org.apache.spark.SparkContext
-import org.apache.spark.graphx.Graph
+import org.apache.spark.graphx._
+
+import scala.collection.mutable
 
 /**
  * Implement Forward-Backward PPR here
@@ -12,9 +16,11 @@ import org.apache.spark.graphx.Graph
  */
 object ForwardBackwardPPRRanking {
 
-  //TODO: Implement functions to run F-B PPR
-
-  var graph: Graph = null
+  var graph: Graph = null // original graph
+  // final result {vid:{key1:val1, key2:val2, ...}}
+  val finalResult = mutable.HashMap[VertexId, mutable.HashMap[String, String]]()
+  // Keys that we will write
+  val stringKeys = Seq("title", "fppr_score", "fppr_rank", "bppr_score", "bppr_rank")
 
   /**
    * Load graph and save to graph variable
@@ -30,6 +36,32 @@ object ForwardBackwardPPRRanking {
    * Run FBPPR
    */
   def run(): Unit = {
+    //TODO: Load graph
+
+    //TODO: Run F-PPR, get topK result
+
+    //TODO: Run B-PPR on all topK, get result
+
+    //TODO: Combine them together, save to finalResult
+
+    //TODO: Call writeResult to write results
+  }
+
+  /**
+   * Write result to a file
+   * @param filePath output file path
+   */
+  def writeResult(filePath: String): Unit = {
+    val writer = new PrintWriter(new File(filePath))
+    // write csv header
+    writer.write((Seq("id") ++ stringKeys).reduce(_+","+_)+"\n")
+
+    finalResult.map(elem => {
+      val tuple = Seq(elem._1.toString) ++ stringKeys.map(x => elem._2.getOrElse(x, "NA").toString)
+      writer.write(tuple.reduce(_+","+_)+"\n")
+    })
+
+    writer.close()
 
   }
 
