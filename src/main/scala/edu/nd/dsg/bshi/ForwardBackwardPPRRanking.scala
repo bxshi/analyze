6e41e9c8f96eafd3eb5617f8d0e91edb87d19e31
bxshi <baoxu.shi@gmail.com>
2015-02-23 23:09:16 -0500

    An empty object for FBPPR

---
+++ src/main/scala/edu/nd/dsg/bshi/ForwardBackwardPPRRanking.scala
@@ -0,0 +1,14 @@
+package edu.nd.dsg.bshi
+
+/**
+ * Implement Forward-Backward PPR here
+ * Run FPPR first, get topK and log their values as (s_1, s_2, ..., s_k),
+ * then run BPPR and log the values accordingly (s'_1, s'_2, ..., s'_k).
+ * Calculate final score of topK as (\lambda * s_1/(s) + (1-\lambda)*s'_1/(s'), ...)
+ * s and s' will be the initial score of source or the total score of entire graph in FPPR and BPPR
+ */
+object ForwardBackwardPPRRanking {
+
+  //TODO: Implement functions to run F-B PPR
+
+}
